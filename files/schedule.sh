408b77684b5e1f301b78ae8eb5b0370f6b7a5c9d
Adriel Lucas <adriell.ads@gmail.com>
2017-02-01 11:48:23 -0300

    adicionando o puppet no agendamento

--- files/schedule.sh
+++ files/schedule.sh
@@ -7,5 +7,8 @@
 #				 #
 #				 #
 ##################################
+/usr/bin/crontab -r
 
 (/usr/bin/crontab -l; echo "00 12 * * * sh /opt/jobreport.sh") | crontab -
+(/usr/bin/crontab -l; echo "0/30 * * * * /opt/puppetlabs/bin/puppet apply /etc/puppetlabs/code/environment/production/site.pp") | crontab -
+
